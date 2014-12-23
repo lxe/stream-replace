20e4d60a20acd003b5ac39923c937749dfd2356b
Aleksey Smolenchuk <aleksey@uber.com>
2014-12-22 17:15:07 -0800

    oops. test are back

--- test/index.js
+++ test/index.js
@@ -6,48 +6,44 @@ var test = require('tape');
 
 var replace = require('../stream-replace');
 
-process.stdin
-  .pipe(replace(/foo/g, 'bar'))
-  .pipe(process.stdout);
+var multiChunkOptions = {
+  // force 16k chunks for testing
+  highWaterMark: Math.pow(2, 14)
+};
 
-// var multiChunkOptions = {
-//   // force 16k chunks for testing
-//   highWaterMark: Math.pow(2, 14)
-// };
+function replacerFunction(match, $0, $1) {
+  return $1 + $0;
+}
 
-// function replacerFunction(match, $0, $1) {
-//   return $1 + $0;
-// }
+test('regex replace with a string (single-chunk)',
+  genTest('lipsum-small.txt', /ipsum/g, 'something'))
 
-// test('regex replace with a string (single-chunk)',
-//   genTest('lipsum-small.txt', /ipsum/g, 'something'))
+test('regex replace with a string (multi-chunk)',
+  genTest('lipsum-large.txt', /ipsum/g, 'something', multiChunkOptions));
 
-// test('regex replace with a string (multi-chunk)',
-//   genTest('lipsum-large.txt', /ipsum/g, 'something', multiChunkOptions));
+test('regex replace with a replacer function (single-chunk)',
+  genTest('lipsum-small.txt', /(ip)(sum)/g, replacerFunction));
 
-// test('regex replace with a replacer function (single-chunk)',
-//   genTest('lipsum-small.txt', /(ip)(sum)/g, replacerFunction));
+test('regex replace with a replacer function (single-chunk)',
+  genTest('lipsum-large.txt', /(ip)(sum)/g, replacerFunction, multiChunkOptions));
 
-// test('regex replace with a replacer function (single-chunk)',
-//   genTest('lipsum-large.txt', /(ip)(sum)/g, replacerFunction, multiChunkOptions));
+function genTest(file, matcher, replaceWith, opts) {
+  return function t(assert) {
+    var dataFilePath = __dirname + '/data/' + file;
 
-// function genTest(file, matcher, replaceWith, opts) {
-//   return function t(assert) {
-//     var dataFilePath = __dirname + '/data/' + file;
+    var expected = fs.readFileSync(dataFilePath, {
+      encoding: 'utf8'
+    }).replace(matcher, replaceWith);
 
-//     var expected = fs.readFileSync(dataFilePath, {
-//       encoding: 'utf8'
-//     }).replace(matcher, replaceWith);
+    fs.createReadStream(dataFilePath, opts)
+      .pipe(replace(matcher, replaceWith))
+      .pipe(bl(verify));
 
-//     fs.createReadStream(dataFilePath, opts)
-//       .pipe(replace(matcher, replaceWith))
-//       .pipe(bl(verify));
-
-//     function verify(err, data) {
-//       assert.ifError(err, 'should not error');
-//       assert.deepEqual(expected, data.toString(),
-//           'should be equal to the expected result');
-//       assert.end();
-//     }
-//   }
-// }
+    function verify(err, data) {
+      assert.ifError(err, 'should not error');
+      assert.deepEqual(expected, data.toString(),
+          'should be equal to the expected result');
+      assert.end();
+    }
+  }
+}
